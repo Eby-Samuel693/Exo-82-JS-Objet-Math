973d67252fc3678ad39b181796338defa914b383
Eby-Samuel693 <coquelet.samuel@gmail.com>
2020-12-14 12:39:47 +0100

    likujnyhbg

---
+++ exo.js
@@ -0,0 +1,42 @@
+// 1. Arrondissez à l'entier inférieur la valeur 34,589456
+let number1 =Math.ceil(34.589456);
+console.log(number1);
+//     2. Arrondissez à l'entier inférieur la valeur 68,412784
+let number2 =Math.ceil(68.412784);
+console.log(number2);
+//     3. Arrondizzez à l'entier supérieur la valeur 39,456
+let number3 = Math.round(39.4560);
+console.log(number3);
+//     4. Arrondissez à l'entier supérieur la valeur 27.34876541
+let number4 = Math.round(27.34876541);
+console.log(number4);
+//     5. Arrondissez à l'entier supérieur ou inférieur ( round ) la valeur 5.68
+let number5 = Math.floor(5.68);
+console.log(number5);
+//     6. Arrondissez à l'entier supérieur ou inférieur ( round ) la valeur 5.41
+let number6 = Math.floor(5.41);
+console.log(number6);
+//     7. Supprimez la partie décimale de la valeur 6.12457
+let number7 = Math.trunc(6.123457);
+console.log(number7);
+//     8. Supprimez la partie décimale de la valeur 9.568124
+let number8 = Math.trunc(9.568124);
+console.log(number8);
+//     9. Affichez en console, la valeur minimale de ces valeurs 5, 1, 12, -1, 45 ( avec une fonction ;-) )
+let number9 = Math.min( 5, 1, 12, -1, 45);
+console.log(number9);
+//     10. Affichez en console, la valeur minimale de ces valeurs 9, 12, 16, 44, 3 ( avec une fonction ;-) )
+let number10 = Math.min( 9, 12, 16, 44, 3);
+console.log(number10);
+//     11. Affichez en console, la valeur maximale de ces valeurs 5, 1, 12, -1, 45 ( avec une fonction ;-) )
+let number11 = Math.max(5, 1, 12, -1, 45);
+console.log(number11);
+//     12. Affichez en console, la valeur maximale de ces valeurs 9, 12, 16, 44, 3 ( avec une fonction ;-) )
+let number12 = Math.max(9, 12, 16, 44, 3);
+console.log(number12);
+//     13. Affichez en console la valeur absolue de la valeur -6
+let number13 = Math.abs(-6);
+console.log(number13);
+//     14. Affichez en console la valeur absolue de la valeur -56
+let number14 = Math.abs(-56);
+console.log(number14);
